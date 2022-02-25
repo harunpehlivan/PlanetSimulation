68f354f3842b3b1ba0e771dd1b65cf1b73384692
HARUN PEHLİVAN <trcmnhp@hotmail.com>
2022-02-25 21:09:55 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import pygame
+import math
+pygame.init()
+
+WIDTH, HEIGHT =  800, 800
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Planet Simulation")
+
+WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
+BLUE = (100, 149, 237)
+RED = (188, 39, 50)
+DARK_GREY = (80, 78, 81)
+
+FONT = pygame.font.SysFont("comicsans", 16)
+
+class Planet:
+	AU = 149.6e6 * 1000
+	G = 6.67428e-11
+	SCALE = 250 / AU  # 1AU = 100 pixels
+	TIMESTEP = 3600*24 # 1 day
+
+	def __init__(self, x, y, radius, color, mass):
+		self.x = x
+		self.y = y
+		self.radius = radius
+		self.color = color
+		self.mass = mass
+
+		self.orbit = []
+		self.sun = False
+		self.distance_to_sun = 0
+
+		self.x_vel = 0
+		self.y_vel = 0
+
+	def draw(self, win):
+		x = self.x * self.SCALE + WIDTH / 2
+		y = self.y * self.SCALE + HEIGHT / 2
+
+		if len(self.orbit) > 2:
+			updated_points = []
+			for point in self.orbit:
+				x, y = point
+				x = x * self.SCALE + WIDTH / 2
+				y = y * self.SCALE + HEIGHT / 2
